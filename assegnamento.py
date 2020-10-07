f99a94ed246b3c1915c5a0a1bb580c5669099887
mariseco <marisecondo@gmail.com>
2020-10-07 17:25:42 +0200

    Ho copiato lo script già fatto in classe, ma è uno script incompleto e da capire aggiungendo commenti

--- assegnamento.py
+++ assegnamento.py
@@ -1,31 +1,46 @@
-""" First assigment.
-""" # i doct strings, racchiusi tra tre pedici doppi, sono cose importanti
+"""First assignment.
+"""
+
 import argparse
 import logging
 import time
-
+import string
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def process(file_path):
-    """ Read a text file and compile the letter statistics."""
+    """Reads a text file and compile the letter statistics."""
     start_time = time.time()
-    logging.info("Reading input file %s. . .", file_path)
+
+    logging.info("Reading input file %s...", file_path)
     with open(file_path) as input_file:
         text = input_file.read()
+    num_chars = len(text)
+    logging.info("Done, %d characters found.", num_chars)
+
+    # char_dict = {chr(x): 0 for x in range(ord('a'), ord('z') + 1)}
+    char_dict = {ch: 0 for ch in string.ascii_lowercase}
+    for ch in text:
+        ch = ch.lower()
+        if ch in char_dict:
+            char_dict[ch] += 1
+        # try:
+        #     char_dict[ch.lower()] += 1
+        # except KeyError:
+        #     pass
+
+    elapsed_time = time.time() - start_time
+    logging.info("Done in %.3f seconds.", elapsed_time)
+    num_letters = sum(char_dict.values())
+    for ch, num in char_dict.items():
+        print(f"{ch} -> {num / num_letters:.3%}")
 
 
-
-    #il logging è fatto per stampare messaggi sopra un certo livello.. se un messaggio non lo stampiamo è inutile questo lavoro! Per stampare qualcosa dobbiamo impostare il logging a un livello più alto 36:00 vedi cos'è pylint.. digiti sul terminale - pylint assegnamento1.py-  ascolta a 46:00
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=str, help="Path to the input file")
     args = parser.parse_args()
-    #30:00
 
     process(args.infile)
 
-#chiamare le variabili in maniera espressiva
-# la funzione ord('val') mi dà il valore di val in tabella ASCII
-#char(val) mi dà il valore che corrisponde nella tabella ASCII a un mio carattere
